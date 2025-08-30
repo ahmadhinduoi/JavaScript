ac64372d2a852399df5f54e4989a88995a4295dd
ahmadhinduoi <154060069+ahmadhinduoi@users.noreply.github.com>
2025-08-30 23:54:29 +0300

    Create if.js
    تاسك/30/8

---
+++ js/if.js
@@ -0,0 +1,16 @@
+
+let corsa1=110
+let corsa2=59
+let corsa3=59
+let corsa4=59
+ 
+if(corsa1 >= 60 && corsa2 >= 60 && corsa3 >= 60 && corsa4 >= 60){
+  console.log("a");}
+  if (corsa1 >= 60 &&  corsa2 >= 60 && corsa3 >= 60 && corsa4 == 59) {
+    console.log("b")}
+     if (corsa1 >= 60 && corsa2>= 60 && corsa3>= 60 &&corsa4== 58) {
+    console.log("d")}
+        if (corsa1>= 60 && corsa2>= 60 &&corsa3== 59 &&corsa4== 59) {
+    console.log("g")}
+           if (corsa1 >= 60 &&corsa2== 59 &&corsa3== 59 &&corsa4 == 59) {
+    console.log("h")
